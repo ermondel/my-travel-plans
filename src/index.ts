cff714196bcb8cbe57a7697b7706ed75f067ae8d
Ermondel <ermondel@gmail.com>
2019-09-06 11:51:12 +0300

    feat: sorting implementation

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,23 @@
-console.log('hi there');
+class Sorter {
+  constructor(public collection: number[]) {}
 
-const logSomething = () => {
-  console.log('Something');
-};
+  sort(): void {
+    const { length } = this.collection;
 
-logSomething();
-logSomething();
-logSomething();
+    for (let i = 0; i < length; i++) {
+      for (let j = 0; j < length - i - 1; j++) {
+        const a = this.collection[j];
+        const b = this.collection[j + 1];
+        if (a > b) {
+          this.collection[j] = b;
+          this.collection[j + 1] = a;
+        }
+      }
+    }
+  }
+}
+
+const sorter = new Sorter([10, 3, -5, 0]);
+sorter.sort();
+
+console.log(sorter.collection);
